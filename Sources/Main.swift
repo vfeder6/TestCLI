4d829419cdc6ffddd5a766607e345bf8df76d4d8
Federico Veronesi <federicoveronesi6@gmail.com>
2024-03-08 14:08:16 +0100

    test

--- Sources/Main.swift
+++ Sources/Main.swift
@@ -5,7 +5,32 @@ import Foundation
 struct Main: ParsableCommand {
 
     public func run() throws {
-        print("THIOKANEEEEE")
-        print(FileManager().currentDirectoryPath)
+        let command = try run(command: "pwd")
+        print(command.0)
+        print(command.1)
+        let command2 = try run(command: "ls -la")
+        print(command2.0)
+        print(command2.1)
+    }
+
+    @discardableResult
+    private func run(command: String) throws -> (String, Int32) {
+        let task = Process()
+        let pipe = Pipe()
+
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["--login", "-c", command]
+        task.executableURL = URL(fileURLWithPath: "/bin/zsh")
+        task.standardInput = nil
+
+        try task.run()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+
+        task.waitUntilExit()
+
+        return (output, task.terminationStatus)
     }
 }
---
+++ test.rb
@@ -0,0 +1,21 @@
+#import the xcodeproj ruby gem
+require ‘xcodeproj’
+
+#define the path to your .xcodeproj file
+project_path = ‘ProjectDir/TestProject.xcodeproj’
+
+#open the xcode project
+project = Xcodeproj::Project.open(project_path)
+
+#find the group on which you want to add the file
+group = project.main_group["TestProject"]["TestGroup"]
+
+#get the file reference for the file to add
+file = group.new_file("TestFile.swift")
+
+#add the file reference to the projects first target
+main_target = project.targets.first
+main_target.add_file_references([file])
+
+#finally, save the project
+project.save
