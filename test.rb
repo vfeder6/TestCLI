55f04e68db8902f062a3046ebc780391a25ccc6b
Federico Veronesi <federicoveronesi6@gmail.com>
2024-03-08 14:21:53 +0100

    Update

--- test.rb
+++ test.rb
@@ -2,13 +2,13 @@
 require ‘xcodeproj’
 
 #define the path to your .xcodeproj file
-project_path = ‘ProjectDir/TestProject.xcodeproj’
+project_path = 'MyWODs.xcodeproj’
 
 #open the xcode project
 project = Xcodeproj::Project.open(project_path)
 
 #find the group on which you want to add the file
-group = project.main_group["TestProject"]["TestGroup"]
+group = project.main_group["MyWODs"]["MyWODs"]
 
 #get the file reference for the file to add
 file = group.new_file("TestFile.swift")
