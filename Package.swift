2b63924aa7fc94d4bb2a0c14c3a35be36265b361
Federico Veronesi <federicoveronesi6@gmail.com>
2024-03-11 11:15:13 +0100

    new

--- Package.swift
+++ Package.swift
@@ -1,19 +1,25 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
-    name: "CLITest",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
+    name: "TestCLI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TestCLI",
+            targets: ["TestCLI"]
+        )
     ],
     targets: [
-        .executableTarget(
-            name: "CLITest",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ],
-            path: "Sources"
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TestCLI"
+        ),
+        .testTarget(
+            name: "TestCLITests",
+            dependencies: ["TestCLI"]
         ),
     ]
 )
---
+++ Sources/TestCLI/TestCLI.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+@main
+struct Main {
+
+    static func main() {
+        print("Hello, world!")
+    }
+}
